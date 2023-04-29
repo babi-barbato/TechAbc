1b8a3c2dcf187733b71178994185f7e3b9c820b6
babi-barbato <barbara.oliveira@sptech.school>
2023-04-28 22:47:43 -0300

    novos testes

--- pages/produtosGerais.php
+++ pages/produtosGerais.php
@@ -9,7 +9,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" /><!-- Link dos icones -->
         <meta name="viewport" content="width=device-width, initial-scale=1.0"><!-- compativel com outros navegadores -->
         <link rel="shortcut icon" href="../img/icon.ico" type="image/x-icon"><!-- icone que aparece na aba da página -->
-        <link rel=
+        <link rel="stylesheet" href="../assets/css/produtos_Gerais-style.css">
         <link rel="stylesheet" href="../assets/css/menu-style.css"><!-- css do menu -->
         <link rel="stylesheet" href="../assets/css/footer-style.css"><!-- css do footer -->
         <title>Produtos Gerais</title><!-- titulo que aparece na aba do site -->
