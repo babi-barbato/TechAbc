4df6d85b7f9c132b5bd40324be2b7afae2fc1253
camilafreo <camila.afanasiev@scseduca.com.br>
2023-05-09 18:48:30 -0300

    Paginas de cadastro

--- assets/js/cadastro_endereco.js
+++ assets/js/cadastro_endereco.js
@@ -13,10 +13,39 @@ function pagar(){
         pix.style.display = "none";
         boleto.style.display = "flex";
     }
+   
 
-    var tamanho = window.body.style.width;
-    if('800px' <= tamanho){
-        esquerda.style.display = "none";
+   
+}
+
+var verifica = 0;
+function cas(){
+    if(verifica == 0){
+        casa.style.color = "white";
+        casa.style.backgroundColor = "#D75A00";
+        verifica = 1;
+        trabalho.disabled = true;
+
+    } else{
+        casa.style.color = "#D75A00";
+        casa.style.backgroundColor = "white";
+        verifica = 0;
+        trabalho.disabled = false;
     }
 
+}
+var veri = 0;
+function trampo(){
+    if(veri == 0){
+        trabalho.style.color = "white";
+        trabalho.style.backgroundColor = "#D75A00";
+        veri = 1;
+        casa.disabled = true;
+      
+    } else{
+        trabalho.style.color = "#D75A00";
+        trabalho.style.backgroundColor = "white";
+        veri = 0;
+        casa.disabled = false;
+    }
 }
