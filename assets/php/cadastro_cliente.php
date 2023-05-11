f0ae127614d51bef135a77b49969869f00a299fd
camilafreo <camila.afanasiev@scseduca.com.br>
2023-05-10 22:38:59 -0300

    Cadastros

--- assets/php/cadastro_cliente.php
+++ assets/php/cadastro_cliente.php
@@ -1,50 +1,50 @@
 <?php
- include('conexao.php'); //Inclui a conexão 
  session_start(); //inicia uma sessão (isso permte saber se está logado ou não)
-
-if (isset($_POST["cadastrar"])){
+ 
+ if (isset($_POST["cadastrar"])){
+    include('conexao.php'); //Inclui a conexão 
     $nome=$_POST['nome'];
     $cpf=$_POST['cpf'];
     $telefone=$_POST['telefone'];
     $email=$_POST['email'];
     $senha=$_POST['senha'];
-    $conf_senha = $_POST['conf'];
+    
 
 
-    $sqli_email= "SELECT * FROM cadastro WHERE email = '$email'";
-    $result = mysqli_query($conexao,$sql_email);
+    $sqli_email= "SELECT * FROM cliente WHERE email = '$email'";
+    $result1 = mysqli_query($conexao,$sql_email);
         
-    if (mysqli_num_rows($result) != 0){  
+    if (mysqli_num_rows($result1) != 0){  
      
         echo "<script>alert ('email ja cadastrado')</script>";
 
-            while ($linha = mysqli_fetch_array($result)){
+            while ($linha = mysqli_fetch_array($result1)){
                 if ($cpf == $linha['cpf']){
                     echo "<script>alert ('cpf ja cadastrado')</script>";} 
           
             }
     }
 
-    $sql_cpf = "SELECT * FROM cadastro WHERE cpf = '$cpf'";
+    $sql_cpf = "SELECT * FROM cliente WHERE cpf = '$cpf'";
     $result2 = mysqli_query($conexao,$sql_cpf);
     if(mysqli_num_rows($result2) != 0){
         echo "<script> alert (' cpf ja cadastrado')</script>";
         
         echo "<script> window.location='../../pages/login.html'</script>"; 
 
-    }
+    
                             
 }else{
-    $sql_cpf = "SELECT * FROM cadastro WHERE cpf = '$cpf'";
+    $sql_cpf = "SELECT * FROM cliente WHERE cpf = '$cpf'";
     $result2 = mysqli_query($conexao,$sql_cpf);
     if(mysqli_num_rows($result2) != 0){
-        echo "<script> alert (' cpf ja cadastrado, cadastrar novo email')</script>"; 
-    
-}else {         
-      $sql=" INSERT INTO cadastro (nome, cpf, telefone, email, senha) VALUES ('$nome', '$cpf','$telefone', '$email', '$senha')";
+        echo "<script> alert (' cpf ja cadastrado, cadastrar novo email')</script>"; }
+      else {         
+      $sql="INSERT INTO clientes (`nome`, `cpf`, `email`, `telefone`, `senha`) VALUES ('$nome', '$cpf','$email','$telefone','$senha')";
     $result=mysqli_query($conexao,$sql);
-    echo "<script> window.location='login.html'</script>"; }
+    echo "<script> window.location.href=' ../html/login.php'</script>"; }
+      
       }
-
+    }
     
 ?>
--- assets/php/conexao.php
+++ assets/php/conexao.php
@@ -1,6 +1,6 @@
 <?php
     $usuario = "root"; //Nome do usuário
     $senha = ""; //Senha do BD
-    $banco = "vet-abc"; //Nome do banco
+    $banco = "techabc"; //Nome do banco
     $conexao=mysqli_connect("localhost",$usuario,$senha,$banco); //realiza de fato a conexão com o banco de dados
 ?>
