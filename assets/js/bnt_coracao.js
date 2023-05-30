af70e175201d2adf77e9889b62ee4d89dd6f7111
camilafreo <camila.afanasiev@scseduca.com.br>
2023-05-29 22:38:18 -0300

    atualizações

--- assets/js/bnt_coracao.js
+++ assets/js/bnt_coracao.js
@@ -2,12 +2,12 @@ var verifica = 0;
 function preenchimento(){
     
     if (verifica == 0){
-        document.getElementById("coracao").classList.remove("fa-regular");
-        document.getElementById("coracao").classList.add("fa-solid");
+        document.getElementById("preenchido").classList.remove("fa-regular");
+        document.getElementById("preenchido").classList.add("fa-solid");
         verifica = 1;
     } else{
-        document.getElementById("coracao").classList.remove("fa-solid");
-        document.getElementById("coracao").classList.add("fa-regular");
+        document.getElementById("preenchido").classList.remove("fa-solid");
+        document.getElementById("preenchido").classList.add("fa-regular");
         verifica = 0;
     }
 }
