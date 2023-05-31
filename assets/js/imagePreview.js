b217247e1a31e0319ca4599c4595cb2d4f2029f5
André Reis <112898914+andre-reis-dev@users.noreply.github.com>
2023-05-31 16:37:08 -0300

    Mais dois scripts comentados

--- assets/js/imagePreview.js
+++ assets/js/imagePreview.js
@@ -11,16 +11,26 @@ inputFile.addEventListener('change', function(e) {
             const reader = new FileReader();
 
             reader.addEventListener('load', function(e) {
+
+                // Verifica se as extensões são algumas das três permitidas
                 if (file.type == "image/png" || file.type == "image/jpeg" || file.type == "image/webp") {
 
+                    // imprime no console a extensão do arquivo selecionado
                     console.log(file.type)
-                    const readerTarget = e.target;
 
+                    // Recebe o target do evento, que é o próprio input file
+                    const readerTarget = e.target;
+                    
+                    // cria o elemento HTML img
                     const img = document.createElement('img');
+                    // define o src da img para o result obtido do upload
                     img.src = readerTarget.result;
+                    // adiciona classe "pictureImg" no elemento, para que receba a devida estilização
                     img.classList.add('pictureImg');
                     
+                    // a label relativa ao input file não recebe texto
                     pictureImage.innerHTML = '';
+                    // a label relativa ao input file recebe a img criada
                     pictureImage.appendChild(img);
 
                 } else {
@@ -28,8 +38,10 @@ inputFile.addEventListener('change', function(e) {
                 }
             });
 
+            // o reader deve ler os dados do objeto file como url
             reader.readAsDataURL(file);
     } else {
+        // Caso não seja feito nenhum upload, a label recebe o texto "Escolha uma imagem"
         pictureImage.innerHTML = pictureImageTxt;
     }
 });
